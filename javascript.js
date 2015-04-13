1d9950455f035e38c45ac98aadc7b247045dce3a
NeilStAmour <neil.stamour@gmail.com>
2015-04-12 22:50:47 -0400

    Final

--- javascript.js
+++ javascript.js
@@ -1,161 +1,117 @@
-var lightingMoneyCost;
-var coolingMoneyCost;
-var heatingMoneyCost;
-var lightingEnergyCost;
-var coolingEnergyCost;
-var heatingEnergyCost;
+var lightingMoneyCost = 0;
+var coolingMoneyCost = 0;
+var heatingMoneyCost = 0;
+var lightingEnergyCost = 0;
+var coolingEnergyCost = 0;
+var heatingEnergyCost = 0;
 
 
 function lightingBest() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.14 * 0;
-	var energyResult = (cost * 1.4 * 0);
-	var lightingMoneyCost = '$' + moneyResult.toFixed(2);
-	var lightingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("lightingMoneyResult").innerHTML = lightingMoneyCost;
-	document.getElementById("lightingEnergyResult").innerHTML = lightingEnergyCost;
+	var efficiency = 0.14 * 0.0;
+	document.getElementById("lights").value = efficiency;
 }
 function lightingBetter() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.14 * 0.25;
-	var energyResult = (cost * 1.4 * 0.25);
-	var lightingMoneyCost = '$' + moneyResult.toFixed(2);
-	var lightingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("lightingMoneyResult").innerHTML = lightingMoneyCost;
-	document.getElementById("lightingEnergyResult").innerHTML = lightingEnergyCost;
+	var efficiency = 0.14 * 0.25;
+	document.getElementById("lights").value = efficiency;
 }
 function lightingAve() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.14 * 0.5;
-	var energyResult = (cost * 1.4 * 0.5);
-	var lightingMoneyCost = '$' + moneyResult.toFixed(2);
-	var lightingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("lightingMoneyResult").innerHTML = lightingMoneyCost;
-	document.getElementById("lightingEnergyResult").innerHTML = lightingEnergyCost;
+	var efficiency = 0.14 * 0.5;
+	document.getElementById("lights").value = efficiency;
 }
 function lightingBad() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.14 * 0.75;
-	var energyResult = (cost * 1.4 * 0.75);
-	var lightingMoneyCost = '$' + moneyResult.toFixed(2);
-	var lightingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("lightingMoneyResult").innerHTML = lightingMoneyCost;
-	document.getElementById("lightingEnergyResult").innerHTML = lightingEnergyCost;
+	var efficiency = 0.14 * 0.75;
+	document.getElementById("lights").value = efficiency;
 }
 function lightingWorst() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.14 * 0.95;
-	var energyResult = (cost * 1.4 * 0.95);
-	var lightingMoneyCost = '$' + moneyResult.toFixed(2);
-	var lightingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("lightingMoneyResult").innerHTML = lightingMoneyCost;
-	document.getElementById("lightingEnergyResult").innerHTML = lightingEnergyCost;
+	var efficiency = 0.14 * 0.95;
+	document.getElementById("lights").value = efficiency;
 }
 
 function coolingBest() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.18 * 0.65;
-	var energyResult = (cost * 1.8 * 0.65);
-	var coolingMoneyCost = '$' + moneyResult.toFixed(2);
-	var coolingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("coolingMoneyResult").innerHTML = coolingMoneyCost;
-	document.getElementById("coolingEnergyResult").innerHTML = coolingEnergyCost;
+	var efficiency = 0.35 * 0.65;
+	document.getElementById("cooling").value = efficiency;
 }
 function coolingBetter() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.18 * 0.55;
-	var energyResult = (cost * 1.8 * 0.55);
-	var coolingMoneyCost = '$' + moneyResult.toFixed(2);
-	var coolingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("coolingMoneyResult").innerHTML = coolingMoneyCost;
-	document.getElementById("coolingEnergyResult").innerHTML = coolingEnergyCost;
+	var efficiency = 0.35 * 0.55;
+	document.getElementById("cooling").value = efficiency;
 }
 function coolingAve() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.18 * 0.45;
-	var energyResult = (cost * 1.8 * 0.45);
-	var coolingMoneyCost = '$' + moneyResult.toFixed(2);
-	var coolingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("coolingMoneyResult").innerHTML = coolingMoneyCost;
-	document.getElementById("coolingEnergyResult").innerHTML = coolingEnergyCost;
+	var efficiency = 0.35 * 0.45;
+	document.getElementById("cooling").value = efficiency;
 }
 function coolingBad() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.18 * 0.35;
-	var energyResult = (cost * 1.8 * 0.35);
-	var coolingMoneyCost = '$' + moneyResult.toFixed(2);
-	var coolingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("coolingMoneyResult").innerHTML = coolingMoneyCost;
-	document.getElementById("coolingEnergyResult").innerHTML = coolingEnergyCost;
+	var efficiency = 0.35 * 0.35;
+	document.getElementById("cooling").value = efficiency;
 }
 function coolingWorst() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.18 * 0.25;
-	var energyResult = (cost * 1.8 * 0.25);
-	var coolingMoneyCost = '$' + moneyResult.toFixed(2);
-	var coolingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("coolingMoneyResult").innerHTML = coolingMoneyCost;
-	document.getElementById("coolingEnergyResult").innerHTML = coolingEnergyCost;
+	var efficiency = 0.35 * 0.25;
+	document.getElementById("cooling").value = efficiency;
 }
 
 
 function heatingBest() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.06 * 0.65;
-	var energyResult = (cost * 0.6 * 0.65);
-	var heatingMoneyCost = '$' + moneyResult.toFixed(2);
-	var heatingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("heatingMoneyResult").innerHTML = heatingMoneyCost;
-	document.getElementById("heatingEnergyResult").innerHTML = heatingEnergyCost;
+	var efficiency = 0.40 * 0.65;
+	document.getElementById("heating").value = efficiency;
 }
 function heatingBetter() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.06 * 0.55;
-	var energyResult = (cost * 0.6 * 0.55);
-	var heatingMoneyCost = '$' + moneyResult.toFixed(2);
-	var heatingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("heatingMoneyResult").innerHTML = heatingMoneyCost;
-	document.getElementById("heatingEnergyResult").innerHTML = heatingEnergyCost;
+	var efficiency = 0.40 * 0.55;
+	document.getElementById("heating").value = efficiency;
 }
 function heatingAve() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.06 * 0.45;
-	var energyResult = (cost * 0.6 * 0.45);
-	var heatingMoneyCost = '$' + moneyResult.toFixed(2);
-	var heatingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("heatingMoneyResult").innerHTML = heatingMoneyCost;
-	document.getElementById("heatingEnergyResult").innerHTML = heatingEnergyCost;
+	var efficiency = 0.40 * 0.45;
+	document.getElementById("heating").value = efficiency;
 }
 function heatingBad() 
 {
-	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.06 * 0.35;
-	var energyResult = (cost * 0.6 * 0.35);
-	var heatingMoneyCost = '$' + moneyResult.toFixed(2);
-	var heatingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("heatingMoneyResult").innerHTML = heatingMoneyCost;
-	document.getElementById("heatingEnergyResult").innerHTML = heatingEnergyCost;
+	var efficiency = 0.40 * 0.35;
+	document.getElementById("heating").value = efficiency;
 }
 function heatingWorst() 
 {
+	var efficiency = 0.40 * 0.25;
+	document.getElementById("heating").value = efficiency;
+}
+
+function calculate()
+{
 	var cost = document.getElementById('cost').value;
-	var moneyResult = cost * 0.06 * 0.25;
-	var energyResult = (cost * 0.6 * 0.25);
-	var heatingMoneyCost = '$' + moneyResult.toFixed(2);
-	var heatingEnergyCost = energyResult.toFixed(0) + 'kWh';
-	document.getElementById("heatingMoneyResult").innerHTML = heatingMoneyCost;
+	
+	var efficiency = document.getElementById('lights').value;
+	var energy = cost * efficiency / 0.187;
+	var money = cost * efficiency;
+	var lightingEnergyCost = energy.toFixed(0) + ' kWh';
+	var lightingMoneyCost = '$' + energy.toFixed(2);
+	document.getElementById("lightingEnergyResult").innerHTML = lightingEnergyCost;
+	document.getElementById("lightingMoneyResult").innerHTML = lightingMoneyCost;
+	
+	efficiency = document.getElementById('cooling').value;
+	energy = cost * efficiency / 0.187;
+	money = cost * efficiency;
+	coolingEnergyCost = energy.toFixed(0) + ' kWh';
+	coolingMoneyCost = '$' + energy.toFixed(2);
+	document.getElementById("coolingEnergyResult").innerHTML = coolingEnergyCost;
+	document.getElementById("coolingMoneyResult").innerHTML = coolingMoneyCost;
+	
+	efficiency = document.getElementById('heating').value;
+	energy = cost * efficiency / 0.187;
+	money = cost * efficiency;
+	heatingEnergyCost = energy.toFixed(0) + ' kWh';
+	heatingMoneyCost = '$' + energy.toFixed(2);
 	document.getElementById("heatingEnergyResult").innerHTML = heatingEnergyCost;
+	document.getElementById("heatingMoneyResult").innerHTML = heatingMoneyCost;
+	
+
 }
