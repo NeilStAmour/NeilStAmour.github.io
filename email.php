e4f90d6480a6c3be202743ac1cef91a14227b650
NeilStAmour <neil.stamour@gmail.com>
2015-07-11 19:31:24 -0400

    Mail changes

--- email.php
+++ email.php
@@ -1,25 +1,28 @@
 <?php
+	$siteKey = '6LcHiAkTAAAAAE4sAYMiuobxfiOsSjtOCRG2rRGo'
+	$secret = '6LcHiAkTAAAAAND2B6zOlUXbEFy_KF3EumsWMec9'
 
         $name;$email;$message;$captcha;
         if(isset($_POST['name']))
-          $name=$_POST['name'];
+			$name=$_POST['name'];
         if(isset($_POST['email']))
-          $email=$_POST['email'];
+			$email=$_POST['email'];
         if(isset($_POST['message']))
-          $message=$_POST['message'];
+			$message=$_POST['message'];
         if(isset($_POST['g-recaptcha-response']))
-          $captcha=$_POST['g-recaptcha-response'];
+			$captcha=$_POST['g-recaptcha-response'];
 
         if(!$captcha){
           echo '<h2>Please check the the captcha form.</h2>';
-          exit;
+				exit;
         }
         $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LcHiAkTAAAAAND2B6zOlUXbEFy_KF3EumsWMec9&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
         if($response.success==false)
         {
-          echo '<h2>You are spammer ! Get the @$%K out</h2>';
+			echo '<h2>You are spammer ! Get the @$%K out</h2>';
         }else
         {
-          echo '<h2>Thanks for posting comment.</h2>';
+			echo '<h2>Thanks for posting comment.</h2>';
+			$sendMail = mail("neil.stamour@gmail.com", "test", $message);
         }
 ?>
